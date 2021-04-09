42b02015a38c0dc7d94e4fe3bb4fdadf5fd648b1
Numericlock <49630768+Numericlock@users.noreply.github.com>
2021-04-09 17:45:33 +0900

    画像を取得し表示

--- layouts/main2.vue
+++ layouts/main2.vue
@@ -8,32 +8,21 @@
                     <v-col xl=2 lg=2 md=2 sm=2 cols=1>
                         <v-sheet min-height="97vh" min-width="90px">
                             <v-list shaped>
-                                <v-list-item v-for="link in links"  link class="my-5">
+                                <v-list-item v-for="link in links" link class="my-5">
                                     <v-list-item-content class="hidden-md-and-down">
                                         <v-list-item-title>{{ link.name }}</v-list-item-title>
                                     </v-list-item-content>
-                                        <v-btn
-                                          class="d-block text-center mx-auto"
-                                          fab
-                                          dark
-                                          color="grey darken-1"
-                                        >
-                                          <v-icon dark>
+                                    <v-btn class="d-block text-center mx-auto" fab dark color="grey darken-1">
+                                        <v-icon dark>
                                             {{ link.icon }}
-                                          </v-icon>
-                                        </v-btn>
+                                        </v-icon>
+                                    </v-btn>
                                 </v-list-item>
                             </v-list>
-                            <v-btn
-                              class="d-block text-center mx-auto"
-                              fab
-                              dark
-                              large
-                              color="cyan"
-                            >
-                              <v-icon dark>
-                                mdi-pencil
-                              </v-icon>
+                            <v-btn class="d-block text-center mx-auto" fab dark large color="cyan">
+                                <v-icon dark>
+                                    mdi-pencil
+                                </v-icon>
                             </v-btn>
                         </v-sheet>
                     </v-col>
@@ -50,44 +39,27 @@
                     </v-col>
 
                     <v-col xl=1 lg=1 md=1 sm=0 cols=0 class="hidden-sm-and-down">
-                        <v-sheet min-height="97vh" min-width="90px" class="mr-1 pt-4">
-                            <v-btn
-                            class="d-block text-center mx-auto" color="grey darken-1"
-                            dark
-                          
-                            
-                            fab
-                            >
-                            <v-icon>mdi-plus</v-icon>
+                        <v-sheet min-height="97vh" max-height="97vh" min-width="90px" class="mr-1 pt-4 overflow-y-auto">
+                            <v-btn class="d-block text-center mx-auto" color="grey darken-1" dark fab>
+                                <v-icon>mdi-plus</v-icon>
                             </v-btn>
                             <v-divider class="mx-3 my-5"></v-divider>
-
-                            <v-avatar v-for="n in 6" :key="n" class="d-block text-center mx-auto mb-9" color="grey lighten-1" size="28">
-                         <!-- <img
-                            v-if="message.avatar"
-                            alt="Avatar"
-                            src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
-                          >
-                          <v-icon
+                            
+                            <v-avatar v-for="list in lists" class="d-block text-center mx-auto mb-9" color="grey lighten-1" size="55">
+                                <img alt="Avatar" class="list-icon" :src="'data:image/png;base64,'+list.icon" />
+                                <!-- <v-icon
                             v-else
                             :color="message.color"
                             v-text="message.icon"
                           ></v-icon>
-    
-                            -->
+            -->
+
                             </v-avatar>
                         </v-sheet>
                     </v-col>
                     <v-col xl=3 lg=3 md=0 sm=0 cols=0 class="hidden-md-and-down">
                         <v-sheet rounded="lg" min-height="97vh" class="mx-1">
-                            <v-select
-                              :items="links"
-                              item-text="name"
-                              item-value="link"
-                              label="Outlined style"
-                              solo
-                              class="mx-1 mt-1"
-                            ></v-select>
+                            <v-select :items="links" item-text="name" item-value="link" label="Outlined style" solo class="mx-1 mt-1"></v-select>
                         </v-sheet>
                     </v-col>
                 </v-row>
@@ -101,34 +73,96 @@
         theme: {
             dark: true
         },
-        async fetch ({ store }) {
-          const list = await store.dispatch('list/fetchList')
-          //console.log("u?:"+list);
-          store.commit('list/setList', list)
+        async fetch({
+            store
+        }) {
+            const list = await store.dispatch('list/fetchList')
+            //console.log("u?:"+list);
+            store.commit('list/setList', list)
         },
         data: () => ({
-            links: [
-                {name: 'Home',link: '/main',icon: 'mdi-home',current:false},
-                {name: 'Search',link: '/main',icon: 'mdi-magnify',current:false},
-                {name: 'Notice',link: '/main',icon: 'mdi-bell',current:false},
-                {name: 'Message',link: '/main',icon: 'mdi-email',current:false},
-                {name: 'Lists',link: '/main',icon: 'mdi-text-box-outline',current:false},
-                {name: 'Profile',link: '/main',icon: 'mdi-account',current:false},
+            links: [{
+                    name: 'Home',
+                    link: '/main',
+                    icon: 'mdi-home',
+                    current: false
+                },
+                {
+                    name: 'Search',
+                    link: '/main',
+                    icon: 'mdi-magnify',
+                    current: false
+                },
+                {
+                    name: 'Notice',
+                    link: '/main',
+                    icon: 'mdi-bell',
+                    current: false
+                },
+                {
+                    name: 'Message',
+                    link: '/main',
+                    icon: 'mdi-email',
+                    current: false
+                },
+                {
+                    name: 'Lists',
+                    link: '/main',
+                    icon: 'mdi-text-box-outline',
+                    current: false
+                },
+                {
+                    name: 'Profile',
+                    link: '/main',
+                    icon: 'mdi-account',
+                    current: false
+                },
             ],
         }),
         computed: {
-        lists() {
-               //console.log("e?:"+this.$store.getters['list/list']);
-              return this.$store.getters['list/list']
-          }
+            lists() {
+                //console.log("e?:"+this.$store.getters['list/list']);
+                return this.$store.getters['list/list']
+            }
         },
-        methods:{
-            
+        methods: {
+
         },
-        async created(){
-              const list = await this.$store.dispatch('list/fetchList')
-             // console.log("u?:"+list);
-              this.$store.commit('list/setList', list) 
+        async created() {
+            const list = await this.$store.dispatch('list/fetchList')
+            // console.log("u?:"+list);
+            this.$store.commit('list/setList', list)
         }
     }
 </script>
+<style lang="scss" scoped>
+    /* スクロールの幅の設定 */
+    *::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+    }
+    /* スクロールの背景の設定 */
+    *::-webkit-scrollbar-track {
+        border-radius: 5px;
+        box-shadow: 0 0 4px #aaa inset;
+    }
+
+    /* スクロールのつまみ部分の設定 */
+    *::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+        background: #666;
+    }
+    .v-avatar{
+        transition:all 0.2s;
+        box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+    }
+    .v-avatar:hover{
+        height:65px !important;
+        width:65px !important;
+        border-radius: 50%;
+        background: #e0e0e0;
+        box-shadow:  7px 7px 14px #969696,
+                     -7px -7px 14px #ffffff;
+        transition:all 0.2s;
+    }
+</style>
--- pages/home.vue
+++ pages/home.vue
@@ -1,24 +1,59 @@
 <template>
-  <section>
-    <h1>管理画面へようこそ</h1>
-  </section>
+    <section class="home-wrapper">
+        <div class="post-container" v-for="post in posts">
+            <div class="user-wrapper" >
+                <v-avatar class="d-block text-center" color="grey lighten-1" size=55>
+                    <img alt="Avatar" class="user-icon" />
+                </v-avatar>
+                <div class="text-xl-h4 text-lg-h6 text-md-body-1 text-sm-body-2 text-caption font-weight-bold my-auto ml-4">{{ post.users_name }}</div>
+
+            </div>
+            <v-carousel class="post-carousel" hide-delimiters v-if="post.attached_count" height="none">
+                <v-carousel-item v-for="images in post.images" :src="'data:image/png;base64,'+images"></v-carousel-item>
+            </v-carousel>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
-  //middleware: 'not_logined_user',
-  //layout: 'home',
-  layout: 'main2',
-  async fetch ({ store }) {
-    const home = await store.dispatch('home/fetchList')
-    //console.log("u?:"+home);
-    store.commit('home/setList', home)
-  },
-  computed: {
-    home () {
-        //console.log("e?:"+this.$store.getters['home/list']);
-        return this.$store.getters['home/list']
+    export default {
+        //middleware: 'not_logined_user',
+        //layout: 'home',
+        layout: 'main2',
+        async fetch({
+            store
+        }) {
+            const home = await store.dispatch('home/fetchList')
+            //console.log("u?:"+home);
+            store.commit('home/setList', home)
+        },
+        computed: {
+            posts() {
+                //console.log("e?:"+this.$store.getters['home/list']);
+                return this.$store.getters['home/list']
+            }
+        },
     }
-  },
-}
 </script>
+<style lang="scss" scoped>
+    .home-wrapper {}
+
+    .post-container {
+        display: flex;
+        flex-direction: column;
+        padding: 20px 30px;
+        background: rgba(62, 62, 62, 0.50);
+        box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+        backdrop-filter: blur(5.0px);
+        -webkit-backdrop-filter: blur(5.0px);
+        border-radius: 10px;
+        border: 1px solid rgba(255, 255, 255, 0.18);
+
+        .user-wrapper {
+            display: flex;
+        }
+    }
+    .post-carousel{
+        height:auto !important;
+    }
+</style>
